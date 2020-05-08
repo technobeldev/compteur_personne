b651ac9e15bed2edbab013da4f9c16238c5d3808
Kevin Decuyper <kevin.decuyper@technobel.be>
2020-05-08 15:03:00 +0200

    add false positives detection in the code

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Compteur de personne
-Compteur de passage permettant de limiter le nombre de personnes dans une pièce
+# Social distancing controller - Covid 19
+Counter to limit the number of people in a room
+
+---
+
+The project is made by Technobel in order to enforce the maximum number of people in a class, in toilets or any room that requires a strict social distancing.
+
+---
+
+The project requires this material:
+* 1 Arduino Uno
+* 2 infrared sensor - sharp 2Y0A21
+* 1 green led
+* 1 red led
+* 1 push button
+* 2 220Ohms resistor
+* cables, breadboard, electrical wire connectors
+
+---
+
+Somes pictures of the prototype:
+
+![Prototype](/pictures/prototype.png "prototype")
+
+and the electric diagram:
+
+![Electric diagram](/electronic/schema.png "prototype")
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -21,12 +21,27 @@ void go(int temp){ //led switch
   }
 }
 
+bool check_measure(int sensor){
+  for (int i=0; i<10; i++){
+    delay(5);
+    int temp1 = analogRead(sensor);
+    if (temp1 <= level){
+      return false;
+    }
+  }
+  return true;
+}
+
 
 void setup()
 {
   Serial.begin(9600); 
   pinMode(green_led, OUTPUT);
   pinMode(red_led, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(7, OUTPUT);
+  digitalWrite(2,HIGH);  
+  digitalWrite(7,HIGH);  
   digitalWrite(green_led,HIGH);  
   digitalWrite(red_led,LOW);            
 }
@@ -37,10 +52,9 @@ void loop()
   val2 = analogRead(sensorpin2); 
 
   //in
-  if (val1 > level && val2 <= level && temp_out == 0 && temp_in == 0){
-    delay(50);
-    val1 = analogRead(sensorpin1); 
-    if (val1 > level){
+  if (val1 > level && val2 <= level && temp_out == 0 && temp_in == 0){ 
+    if (check_measure(sensorpin1)){
+      delay(1000);
       temp_in = 1;
     }
   }
@@ -56,10 +70,9 @@ void loop()
   }
 
   //out
-  if (val2 > level && val1 <= level && temp_in == 0 && temp_out == 0){
-    delay(50);
-    val2 = analogRead(sensorpin2); 
-    if (val2 > level){
+  if (val2 > level && val1 <= level && temp_in == 0 && temp_out == 0){ 
+    if (check_measure(sensorpin2)){
+      delay(1000);
       temp_out = 1;
     }
   }
@@ -74,7 +87,16 @@ void loop()
     }else{
       go(true);
     }
-  }
- 
-  delay(10);                    
+  } 
+  /*
+  if (val1 > 150){
+    delay(10);
+    int temp1 = analogRead(sensorpin1);
+    delay(10);
+    int temp2 = analogRead(sensorpin1);
+    delay(10);
+    int temp3 = analogRead(sensorpin1);
+    if (temp1 >= 150 && temp2 >= 150 && temp3 >= 150)
+    Serial.println(val1);                  
+  }*/
 }
